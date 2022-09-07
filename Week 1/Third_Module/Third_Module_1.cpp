95cac628977dc7e1e7068597c38633c96d5fa095
Nabila Tarannum <89971373+NabilaTarannum@users.noreply.github.com>
2022-09-08 05:45:36 +0600

    First_Week

---
+++ Week 1/Third_Module/Third_Module_1.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    // int ar[5] = {2, 1, 4, 5, 3};
+    // int ar[5];
+    // for (int i = 0; i < 5; i++)
+    // {
+    //     cin >> ar[i];
+    // }
+    // for (int i = 0; i < 5; i++)
+    // {
+    //     cout << ar[i] << " ";
+    // }
+
+    // char ar[5] = "abcd";
+    char ar[50];
+    cin.getline(ar, 50);
+    cout << ar << endl;
+    return 0;
+}
