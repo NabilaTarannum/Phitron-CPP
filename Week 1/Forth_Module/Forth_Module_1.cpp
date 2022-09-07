504fe67b1bc63e0731d4266eaa5c28572034edb2
Nabila Tarannum <89971373+NabilaTarannum@users.noreply.github.com>
2022-09-08 05:49:14 +0600

    First_Week

---
+++ Week 1/Forth_Module/Forth_Module_1.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    cout << "1. square matrix - " << endl << "1 2 3\n4 5 6\n7 8 9" << endl << endl;
+
+    cout << "2. diagonal matrix - | primariy diagonal" << endl << "1 0 0\n0 5 0\n0 0 9" << endl << endl;
+
+    cout << "2. diagonal matrix - || secondary diagonal" << endl << "0 0 3\n0 5 0\n7 0 0" << endl << endl;
+
+    cout << "3. scalar matrix - " << endl << "2 0 0\n0 2 0\n0 0 2" << endl << endl;
+
+    cout << "4. identity matrix - " << endl << "1 0 0\n0 1 0\n0 0 1" << endl << endl;
+
+    cout << "5. symmetric matrix - " << endl << "1 2 3\n2 3 4\n3 4 5" << endl << endl;
+    return 0;
+}
