a89c7807e607531fc86a913631cded1718468414
Nabila Tarannum <89971373+NabilaTarannum@users.noreply.github.com>
2022-09-09 06:42:36 +0600

    Second_Week

---
+++ Week 2/Seventh_Module/Seventh_Module_1.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class BankAccount
+{
+    public:
+        string account_holder;
+        string address;
+        int age;
+        int account_number;
+
+    protected:
+        int balance;
+
+    private:
+        string password;
+
+    public:
+        BankAccount(string account_holder, string address, int age, string password)
+        {
+            this->account_holder = account_holder;
+            this->address = address;
+            this->age = age;
+            this->password = password;
+            this->account_number = rand() % 100000000;
+            cout << "Your account no is " << this->account_number << endl;
+        }
+};
+
+int main()
+{
+    return 0;
+}
